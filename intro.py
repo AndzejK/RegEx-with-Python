cb92139a3925f682af4c43cccbe406fc95ca80fe
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-04-08 11:03:31 +0300

    using compile method to store a pattern

--- intro.py
+++ intro.py
@@ -1,1 +1,6 @@
+# deepnote.com
 import re
+some_str="Hey mate, i've got some news please write me back to this email goodnews@channel.com but don't use this badnews@channel.com email!!"
+# get the emails
+email_pattern=re.compile("[a-z]@[a-z].[a-z]") # thisis  a regular expression object <class 're.Pattern'>
+matches=email_pattern.findall(some_str)
